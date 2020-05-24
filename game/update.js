47b35c03d4d41cfaccb7903c2b5439757e896f39
iceburgy <iceburg2005@hotmail.com>
2020-05-23 23:17:53 -0700

    fix_aocai_not_wuxie

--- game/update.js
+++ game/update.js
@@ -1,5 +1,5 @@
 window.noname_update={
-	version:'1.9.98.7.20200523.2',
+	version:'1.9.98.7.20200523.3',
 	update:'1.9.98.7.20200522',
 	changeLog:[
 		'默认显示“重来”按钮',
@@ -8,6 +8,7 @@ window.noname_update={
 		'默认开启双将模式，以免报错“propery[3] of undefined”',
 		'默认选择布局为新版，以免10人局只能看见2个玩家',
 		'将“十周年UI”嵌入游戏',
+		'修复傲才出无懈',
 		'更多更新历史信息请查看：https://bit.ly/wumingsha',
 	],
 	files:[
